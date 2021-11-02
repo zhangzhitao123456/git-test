9c156a0b1c80d9907fcf4101b7ee2e88063ac727
zzt <zzt@nxf.com>
2021-11-02 17:37:35 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author zhangzhitao
+ * @version JDK_1.8
+ * @create 2021-11-02-17:33
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello  git!");
+    }
+}
